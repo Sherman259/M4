b76c7187fe1fb252db3ee24ad885167f7722a4f5
Sherman259 <jtihomir@gmail.com>
2019-03-27 18:47:20 +0100

    wee

---
+++ 25.2.19/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25._2._19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite prirodan broj:");
+            int a = int.Parse(Console.ReadLine());
+            if (a > 10)
+            {
+
+
+
+
+
+
+
+
+
+       
+
+
+                Console.WriteLine("Uneseni broj je veći od 10.");
+            }
+
+
+
+
+            else
+            {
+
+                Console.WriteLine("Uneseni broj NIJE veći od 10.");
+            }
+
+            Console.ReadKey();
+        }
+
+
+    }
+
+}
---
+++ 25.2.19/primjer 5.1.2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+public class Class1
+{
+    public Class1()
+    {
+        Console.WriteLine("Unesite cijeli broj:");
+        int a = int.Parse(Console.ReadLine());
+        if (a > 0)
+        {
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.WriteLine("Uneseni broj je pozitivan.");
+        }
+
+
+
+
+        else
+        {
+
+
+        }
+        else
+        {
+            Console.WriteLine("Uneseni broj  je jednak 0.");
+        }
+    }
+
+
+            Console.ReadKey();
+    }
+
+
+}
+
+}
+
+	}
+}
